e601b142efad8869e006e70550f5dfb8881b852d
hakanyarimaga1202 <hakanyarimaga1202@hotmail.com>
2020-08-31 22:39:55 +0300

    Add project files.

---
+++ Serialization-Examples/Functions/BinarySerializeDeserialize.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+
+namespace Serialization_Examples.Functions
+{
+    public class BinarySerializeDeserialize
+    {
+        public static byte[] SerializeBinary(object variable1)
+        {
+            using(var stream = new MemoryStream())
+            {
+                var binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(stream, variable1);
+                return stream.ToArray();
+            }
+        }
+
+        public static object DeserializeBinary(byte[] variable1)
+        {
+            using (var stream = new MemoryStream(variable1))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                return binaryFormatter.Deserialize(stream);
+            }
+        }
+
+    }
+}
